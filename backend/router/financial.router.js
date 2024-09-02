50bc9c4d9ffa4359353f6465463c2a8115b637bd
Universeisu <appleapple10092544@gmail.com>
2024-09-02 17:04:47 +0700

    first commit

---
+++ backend/router/financial.router.js
@@ -0,0 +1,20 @@
+const financialController = require("../controllers/financial.controller");
+const express = require("express");
+const { Financial } = require("../models");
+const router = express.Router();
+
+//create a new financial
+router.post("/",financialController.create)
+
+router.get("/", financialController.findAll);
+
+router.get("/user/:userID", financialController.findAllByUserID);
+
+router.get("/:id", financialController.findOne);
+
+router.put("/:id", financialController.update);
+
+router.delete("/:id", financialController.delete);
+
+
+module.exports = router;
